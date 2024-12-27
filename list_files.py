6ee995665cee6f3bdbe31786f5dc6dabc7f71e3a
rasikaprabhath <rasikaprabhath@gmail.com>
2024-12-27 21:43:10 +0530

    2nd version

--- list_files.py
+++ list_files.py
@@ -3,12 +3,13 @@ import os
 folders = input("enter folder names with spaces in between. Pls enter the full path : ").split()
 
 for folder in folders:
+    counter = 1
     try:
         files = os.listdir(folder)
     except FileNotFoundError:
         print(f"pls enter a valid folder with the actual path. Folder  {folder} does not exits")
         break
-    print(files)
 
     for file in files:
-        print(file)
+        print(f"{counter}. {file}")
+        counter = counter + 1
