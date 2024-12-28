d5f605a326d5d039b6a405e2a0f54889811e27b3
rasikaprabhath <rasikaprabhath@gmail.com>
2024-12-28 21:53:25 +0530

    Added comments to the file

---
+++ create_S3_bucket.py
@@ -0,0 +1,12 @@
+# Retrieve the list of existing s3 buckets
+import boto3
+def list_s3_buckets():
+    client = boto3.client('s3')
+    s3cleint= client.list_buckets()
+# Output the bucket names using the "Buckets" parameter
+    print('Existing buckets are:')
+    for param in s3cleint['Buckets']:
+        print(f'Bucket Name : {param["Name"]} - Create Date & Time : {param["CreationDate"]}')
+
+#invoking the bucket function
+list_s3_buckets()
