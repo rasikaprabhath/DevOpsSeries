1a8b671fdf0fcfff4fc5f5732c93fa40afcd7ddc
rasikaprabhath <rasikaprabhath@gmail.com>
2024-12-28 17:54:18 +0530

    Added comments to the file

--- list_files.py
+++ list_files.py
@@ -1,9 +1,17 @@
+'''This peice of code demo how files of  given folders of your local machine is listed.
+you can specify folder name with the full path seperating by spaces
+'''
 import os
-
+#take the input from the CLI. seperate folders with the space.
+# split() will identify the space and split the string
+#folders will store these strings in a list
 folders = input("enter folder names with spaces in between. Pls enter the full path : ").split()
 
+#traversing through the list to find folders
 for folder in folders:
+#counter is used to dispaly the sequence number in front of the file name
     counter = 1
+#error handling for FileNotFound Exception
     try:
         files = os.listdir(folder)
         print(" ")
@@ -13,7 +21,7 @@ for folder in folders:
     except FileNotFoundError:
         print(f"pls enter a valid folder with the actual path. Folder  {folder} does not exits")
         break
-
+#printing the files inside the folder
     for file in files:
         print(f"{counter}. {file}")
         counter = counter + 1
