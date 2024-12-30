ac8cef63e73acb96969e4a74890310c268295250
rasikaprabhath <rasikaprabhath@gmail.com>
2024-12-30 10:47:26 +0530

    first version

--- create_S3_bucket.py
+++ create_S3_bucket.py
@@ -1,12 +1,36 @@
-# Retrieve the list of existing s3 buckets
+import logging
 import boto3
-def list_s3_buckets():
-    client = boto3.client('s3')
-    s3cleint= client.list_buckets()
-# Output the bucket names using the "Buckets" parameter
-    print('Existing buckets are:')
-    for param in s3cleint['Buckets']:
-        print(f'Bucket Name : {param["Name"]} - Create Date & Time : {param["CreationDate"]}')
+from botocore.exceptions import ClientError
 
-#invoking the bucket function
-list_s3_buckets()
+#importing the list_S3_buckets.py file which was already written to list exisitng buckets
+import list_S3_buckets as existings3
+bucketname=existings3.list_s3_buckets()
+print(bucketname)
+
+#function to create a bucket
+def create_s3_bucket():
+
+    client=boto3.client('s3')
+    try:
+        newBucketName=input("enter a unique name to create the S3 bucket : ")
+        if newBucketName!=bucketname:
+            s3client=client.create_bucket(
+                Bucket=newBucketName,
+                CreateBucketConfiguration={
+                    'LocationConstraint': 'ap-south-1'
+                }
+            )
+        else:
+            try:
+                print(f"Given bucket {newBucketName} exists. Pls enter a new name")
+                create_s3_bucket()
+            except BucketAlreadyExists:
+                print(f"Given bucket {newBucketName} exists. Pls enter a new name")
+    except ClientError as e:
+        logging.error(e)
+        return False
+        print(s3client)
+    return True
+
+
+create_s3_bucket()
