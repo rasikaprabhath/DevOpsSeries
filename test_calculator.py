2bf489684f10f3ce158f6ddf2598ea4ea964e9c2
rasikaprabhath <107238623+rasikaprabhath@users.noreply.github.com>
2025-06-20 10:58:08 +1200

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,9 +1,15 @@
 # test_calculator.py
 
-import Calculator
+from calculator_jenkins import add, subtract, multiply, divide
 
 def test_add():
-    assert Calculator.add(2, 3) == 5
+    assert add(3, 5) == 8
 
 def test_subtract():
-    assert Calculator.subtract(5, 3) == 2
+    assert subtract(10, 4) == 6
+
+def test_multiply():
+    assert multiply(3, 3) == 9
+
+def test_divide():
+    assert divide(10, 2) == 5
